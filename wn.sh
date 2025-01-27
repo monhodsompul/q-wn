55b4b4719046bb707b3ff359242f5fa75dff3b64
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2025-01-27 14:23:15 +0700

    Update wn.sh

--- wn.sh
+++ wn.sh
@@ -1,3 +1,5 @@
+cd /root
+
 wget -O bios64.bin "https://github.com/BlankOn/ovmf-blobs/raw/master/bios64.bin" 
 
 wget -O win.iso "https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US" 
@@ -8,7 +10,9 @@ tar -xf ngrok.tgz
 
 ./ngrok config add-authtoken 2hw2qcFisyQHN5js4b0Luy6SaPL_JZd9EBYVYog8zwxzqyVs
 
-./ngrok tcp 5900 
+nohup ./ngrok tcp 5900 > /dev/null 2>&1 &
+
+mkdir tes && cd tes && wget -O ngrok.tgz "https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz" && tar -xf ngrok.tgz && ./ngrok config add-authtoken 22G2Pd4rURud2rTWDk4jtva8WX5_73D2vf71zciBDX1M4cJyG && nohup ./ngrok tcp 3389 > /dev/null 2>&1 &
 
 sudo apt update 
 
@@ -17,7 +21,7 @@ sudo apt install qemu-kvm -y
 qemu-img create -f raw memek 15G 
 
 qemu-system-x86_64 \
-  -m 10G \
+  -m 12G \
   -smp cores=3 \
   -boot order=c \
   -drive file=win.iso,media=cdrom \
